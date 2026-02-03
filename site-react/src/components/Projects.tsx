a0d91eb12d1531cbd6caec598217251cadae0118
vladfiffie <vladimirfiffiejr@proton.me>
2026-02-03 15:44:41 -0600

    feat: Added In Progress Section

--- site-react/src/components/Projects.tsx
+++ site-react/src/components/Projects.tsx
@@ -1,33 +1,37 @@
 "use client";
 
-
 import { FocusCards, CardType } from "@/components/ui/focus-cards";
+
 export default function Projects() {
+  const cards: CardType[] = [
+    {
+      title: "View on Github",
+      src: "/images/paoer-ship.jpg",
+      ctaLink: "https://github.com/ponderrr/paoer_ship",
+      aspect: "landscape",
+    },
+    {
+      title: "View on Github",
+      src: "/images/lion-den-cinema.png",
+      ctaLink: "https://github.com/ponderrr/lions-den-cinema",
+      aspect: "landscape",
+    },
+    {
+      title: "Visit Website",
+      src: "/images/smart-advisor-transparent.png",
+      ctaLink: "https://smartadvisor.live/",
+      aspect: "landscape",
+    },
+  ];
 
-const cards: CardType[] = [
-  {
-    title: "View on Github",
-    src: "/images/paoer-ship.jpg",
-    ctaLink: "https://github.com/ponderrr/paoer_ship",
-    aspect: "landscape",
-  },
-  {
-    title: "View on Github",
-    src: "/images/lion-den-cinema.png",
-    ctaLink: "https://github.com/ponderrr/lions-den-cinema",
-    aspect: "landscape",
-  },
-  {
-    title: "Visit Website",
-    src: "/images/smart-advisor-transparent.png",
-    ctaLink: "https://smartadvisor.live/",
-    aspect: "landscape",
-  },
-
-];
-
-
-
+  const inProgressCards: CardType[] = [
+    {
+      title: "Ecommerce Website",
+      src: "/images/placeholder.png", 
+      aspect: "landscape",
+    },
+   
+  ];
 
   return (
     <section
@@ -39,10 +43,14 @@ const cards: CardType[] = [
         <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-16 dark:text-white">
           Featured Projects
         </h2>
-
         <FocusCards cards={cards} />
+
+        {/* In Progress Section */}
+        <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center mt-32 mb-16 dark:text-white">
+          Projects In Progress
+        </h2>
+        <FocusCards cards={inProgressCards} />
       </div>
     </section>
   );
 }
-
